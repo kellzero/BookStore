8ee0b523b7bb7c99a24c943544f013e9859fc46b
kelvin <kellzer01@gmail.com>
2026-01-07 11:06:11 -0300

    correçao exercicio

--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -1,9 +1,12 @@
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from order.models import Order
 from order.serializers import OrderSerializer
 
 class OrderViewSet(ModelViewSet):
-
+    authentication_classes = [SessionAuthentication, TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     serializer_class = OrderSerializer
     queryset = Order.objects.all()
